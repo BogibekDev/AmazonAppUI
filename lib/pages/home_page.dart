f8bef1ee154578e050a485a169defddf1b4f18ba
bogibekdev <bogibekdev@gmail.com>
2023-05-12 21:42:06 +0500

    has bug

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,6 +10,18 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xff01B197),
+        title: Row(children: [
+          Container(
+            width: 100,
+            child: const Image(
+              image: AssetImage('assets/images/ic_amazon.png'),
+            ),
+          )
+        ]),
+      ),
+    );
   }
 }
