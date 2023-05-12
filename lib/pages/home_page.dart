1160cefcff1b6724fca9a09db1f9867a572330b1
bogibekdev <bogibekdev@gmail.com>
2023-05-12 22:09:25 +0500

    appbar and deliver finished

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,16 +12,111 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 0,
         backgroundColor: const Color(0xff01B197),
-        title: Row(children: [
-          Container(
-            width: 100,
-            child: const Image(
-              image: AssetImage('assets/images/ic_amazon.png'),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const SizedBox(
+              width: 100,
+              child: Image(
+                image: AssetImage('assets/images/ic_amazon.png'),
+              ),
             ),
-          )
-        ]),
+            Row(
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.mic,
+                    color: Colors.white,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.shopping_cart,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
+      body: Container(
+        color: Colors.grey.shade300,
+        child: Column(
+          children: [
+            //#search
+            Container(
+              padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
+              color: const Color(0xff01B197),
+              child: Container(
+                height: 50,
+                padding: const EdgeInsets.only(left: 10, right: 10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  color: Colors.white,
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 1,
+                  ),
+                ),
+                child: const Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: "Navvi xolisan",
+                          border: InputBorder.none,
+                          icon: Icon(
+                            Icons.search,
+                            color: Color(0xff01B197),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Icon(
+                      Icons.camera_alt,
+                      color: Color(0xff01B197),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+                child: ListView(
+              children: [
+                //location
+                Container(
+                  padding: const EdgeInsets.only(left: 10, right: 10),
+                  color: Colors.blueGrey,
+                  height: 45,
+                  child: const Row(
+                    children: [
+                      Icon(
+                        Icons.location_on,
+                        color: Colors.white,
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        "Deliver to Karvak , Khorezm",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ))
+          ],
+        ),
+      ),
+      drawer: const Drawer(),
     );
   }
 }
