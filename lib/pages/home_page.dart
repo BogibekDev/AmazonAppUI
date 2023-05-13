84e13a4ca7aeda6189cc6756ae4d4f6fbd2a983d
bogibekdev <bogibekdev@gmail.com>
2023-05-13 10:45:14 +0500

    ads and sign created

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -40,7 +40,7 @@ class _HomePageState extends State<HomePage> {
                   ),
                 ),
               ],
-            )
+            ),
           ],
         ),
       ),
@@ -86,33 +86,102 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
             Expanded(
-                child: ListView(
-              children: [
-                //location
-                Container(
-                  padding: const EdgeInsets.only(left: 10, right: 10),
-                  color: Colors.blueGrey,
-                  height: 45,
-                  child: const Row(
-                    children: [
-                      Icon(
-                        Icons.location_on,
-                        color: Colors.white,
-                      ),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      Text(
-                        "Deliver to Karvak , Khorezm",
-                        style: TextStyle(
+              child: ListView(
+                children: [
+                  //location
+                  Container(
+                    padding: const EdgeInsets.only(left: 10, right: 10),
+                    color: Colors.blueGrey,
+                    height: 45,
+                    child: const Row(
+                      children: [
+                        Icon(
+                          Icons.location_on,
                           color: Colors.white,
                         ),
-                      )
-                    ],
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text(
+                          "Deliver to Karvak , Khorezm",
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        )
+                      ],
+                    ),
                   ),
-                ),
-              ],
-            ))
+                  //ads
+                  Container(
+                    height: 140,
+                    color: Colors.white,
+                    child: Row(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          width: 140,
+                          child: const Center(
+                            child: Text(
+                              "Biza 45 million maxsulotni\nyetkazib berdik",
+                              style:
+                                  TextStyle(color: Colors.black, fontSize: 16),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            decoration: const BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(70),
+                                bottomLeft: Radius.circular(70),
+                              ),
+                              image: DecorationImage(
+                                  image: AssetImage('assets/images/track.jpg'),
+                                  fit: BoxFit.cover),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 6),
+                  //sign
+                  Container(
+                    color: Colors.white,
+                    height: 160,
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text("Tizimga kirishni tavsiya qilamiz!",
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 18)),
+                        const SizedBox(height: 15),
+                        Container(
+                          width: MediaQuery.of(context).size.width,
+                          height: 50,
+                          color: Colors.orange,
+                          child: const Center(
+                            child: Text(
+                              "Tizimga kirish",
+                              style: TextStyle(color: Colors.white,fontSize: 16),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 15),
+                        const Text("Ro'yhatdan o'tish",
+                            style:
+                            TextStyle(color: Colors.blueAccent, fontSize: 18)),
+                      ],
+                    ),
+                  ),
+                  //List
+                  Container(),
+                ],
+              ),
+            ),
           ],
         ),
       ),
